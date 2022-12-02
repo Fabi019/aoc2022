527969d0642b803408ba1483a175c40827086f3b
Fabi019 <fabi019@gmx.de>
2022-12-02 09:30:04 +0100

    refactor day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,22 +1,20 @@
+use std::collections::BinaryHeap;
+
 static INPUT: &str = include_str!("../../assets/day01.txt");
 
 fn main() {
-  let mut elfs = Vec::new();
+  let mut elfs = BinaryHeap::new();
   let mut current_sum = 0;
 
-  INPUT.lines().for_each(|line| { 
+  for line in INPUT.lines() { 
     if line.is_empty() {
       elfs.push(current_sum);
       current_sum = 0;
     } else {
       current_sum += line.parse::<i32>().unwrap();
     }
-  });
+  }
 
-  println!("Maximum Calories: {}", elfs.iter().max().unwrap());
-
-  elfs.sort();
-  elfs.reverse();
-
+  println!("Maximum Calories: {}", elfs.peek().unwrap());
   println!("Top 3 Total Calories: {}", elfs.iter().take(3).sum::<i32>());
 }
