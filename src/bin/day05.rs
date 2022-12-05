401a00ab694ef6a4b27f45a1d03d81287e3e4b93
Fabi019 <fabi019@gmx.de>
2022-12-05 08:55:33 +0100

    day 5

---
+++ src/bin/day05.rs
@@ -0,0 +1,73 @@
+use std::collections::BTreeMap;
+
+static INPUT: &str = include_str!("../../assets/day05.txt");
+
+fn main() {
+    let mut stacks = BTreeMap::new();
+    let mut start_idx = 1;
+
+    // Parse stacks
+    for line in INPUT.lines() {
+        if line.is_empty() {
+            break;
+        }
+
+        for (idx, _) in line.match_indices('[') {
+            let c = line.chars().nth(idx + 1).unwrap();
+            stacks
+                .entry(((idx + 3) / 4) + 1)
+                .or_insert(Vec::new())
+                .push(c);
+        }
+
+        start_idx += 1;
+    }
+
+    for (_, stack) in stacks.iter_mut() {
+        stack.reverse()
+    }
+
+    println!("Stacks: {:?}", stacks);
+
+    // Move crates
+    for line in INPUT.lines().skip(start_idx) {
+        let mut split = line.split(' ');
+        split.next(); // move
+        let count = split.next().unwrap().parse::<usize>().unwrap();
+        split.next(); // from
+        let from = split.next().unwrap().parse::<usize>().unwrap();
+        split.next(); // to
+        let to = split.next().unwrap().parse::<usize>().unwrap();
+
+        println!("Move {} from {} to {}", count, from, to);
+
+        let mut from_stack = stacks.get(&from).unwrap().clone();
+        let mut to_stack = stacks.get(&to).unwrap().clone();
+
+        // Part one: move one at a time
+        //for _ in 0..count {
+        //    to_stack.push(from_stack.pop().unwrap());
+        //}
+
+        // Part two: move all at once
+        to_stack.append(
+            from_stack
+                .drain((from_stack.len() - count)..)
+                .collect::<Vec<_>>()
+                .as_mut(),
+        );
+
+        stacks.insert(from, from_stack);
+        stacks.insert(to, to_stack);
+    }
+
+    println!("Stacks: {:?}", stacks);
+
+    println!(
+        "Top: {}",
+        stacks
+            .iter()
+            .map(|(_, stack)| stack.last().unwrap())
+            .collect::<String>()
+    );
+}
